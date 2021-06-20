0ef1eb4712d27336db48ab4ad5fcb1e21c393a4c
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-06-20 15:00:50 +0300

    lab01b

---
+++ app.js
@@ -0,0 +1,12 @@
+let car = prompt("wich type of car do you like?")
+alert('Nice')
+console.log(car);
+let age = prompt("How od are you?")
+alert(age + "years old")
+console.log(age);
+let cat = prompt("do you like cats?")
+alert('cute one')
+console.log(cat);
+let food = prompt("wich type of food do yo like?")
+alert('delicious')
+console.log(food);
